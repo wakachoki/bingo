fa29cd42db1c12041a5dde0f9b3dd537ad34794d
Tatsuya Wakatsuki <wakatsuki@chomot.com>
2015-05-20 23:16:19 +0900

    bingo

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja"
+  <head>
+    <meta charset="UTF-8">
+    <title>BINGOシート</title>
+  </head>
+  <style>
+    td, th {
+      width: 50px;
+      border: 1px solid #ccc;
+      text-align: center;
+    }
+  </style>
+  <body>
+    <h1>BINGOシート</h1>
+    <table>
+      <tr><th>B</th><th>I</th><th>N</th><th>G</th><th>O</th></tr>
+    </table>
+  </body>
+</html>
